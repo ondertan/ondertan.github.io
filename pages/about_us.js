23b8a9370bbaad5697e0ec65bfe0b290db966a88
Tanay Onder <tanayonder@gmail.com>
2017-05-11 15:53:55 -0400

    nuke

--- pages/about_us.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    //load navbar
-    $('#navbar_holder').load('/components/navbar.html', function () {
-        $('.site_name').html('<i class="fa fa-info-circle w3-hide-small"></i> About Us <i class="fa fa-info-circle w3-hide-small"></i>');
-    });
-});
